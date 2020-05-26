26b98114351e42e6c5560b8ab0e5f0e0d2b51e14
Riffs9720 <rferlasilva@gmail.com>
2020-05-26 13:01:11 -0300

    revert: implementado a comunicação do frontend com o backend no heroku

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
+/* Uso local
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
+}); */
+
+export const api = axios.create({
+  baseURL: 'https://backend-tcc.herokuapp.com/',
 });
